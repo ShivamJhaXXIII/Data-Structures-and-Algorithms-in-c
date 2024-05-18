22acd768bf2d44461340a2d05d664e1b9f16972c
ShivamJhaXXIII <shivam23040223@gmail.com>
2024-05-18 14:58:26 +0530

    Insertion at the Beginning

--- NesoAcademy/Doubly Linked List/Insertin_Node_in_Empty_List.c
+++ NesoAcademy/Doubly Linked List/Insertin_Node_in_Empty_List.c
@@ -7,6 +7,14 @@ typedef struct node{
     struct node *next;
 } Node;
 
+void print_list(Node *head) {
+    Node *ptr = head;
+    while(ptr != NULL) {
+        printf("%d ", ptr -> data);
+        ptr = ptr -> next;
+    }
+}
+
 Node *addToEmpty(Node *head, int data) {
     Node *temp = malloc(sizeof(Node));
     temp -> prev = NULL;
@@ -15,9 +23,22 @@ Node *addToEmpty(Node *head, int data) {
     head = temp;
     return head;
 }
+
+Node *addAtBeg(Node *head, int data) {
+    Node *temp = malloc(sizeof(Node));
+    temp -> prev = NULL;
+    temp -> data = data;
+    temp -> next = NULL;
+    // Creation of the node done
+    temp -> next = head;
+    head -> prev = temp;
+    head = temp;
+    return head;
+}
 int main () {
     Node *head = NULL;
     head = addToEmpty(head, 45);
-    printf("%d", head -> data);
+    head = addAtBeg(head, 34);
+    print_list(head);
     return 0;
 }
