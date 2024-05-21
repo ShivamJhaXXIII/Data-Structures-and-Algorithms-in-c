6e8cf51cc25168c2b0510d05d60ee025a9b50c91
ShivamJhaXXIII <shivam23040223@gmail.com>
2024-05-21 16:05:47 +0530

    Insertion between the Nodes

--- NesoAcademy/Circular Linked List/CircularSingly.c
+++ NesoAcademy/Circular Linked List/CircularSingly.c
@@ -35,6 +35,30 @@ node *addAtEnd(node *tail, int data)
     return tail;
 }
 
+node *addAtAnyPos(node *tail, int data, int pos) 
+{
+    node *newP = malloc(sizeof(node));
+    newP -> data = data;
+    newP -> next = NULL;
+
+    node *ptr = tail -> next;
+
+    while (pos > 1) {
+        ptr = ptr -> next;
+        pos--;
+    }
+
+    if(ptr == tail) {
+        newP -> next = tail -> next;
+        tail -> next = newP;
+        tail = tail -> next;
+    } else {
+        newP -> next = ptr -> next;
+        ptr -> next = newP;
+    }
+    return tail;
+}
+
 void print(node *tail) 
 {
     node *ptr = tail -> next;
@@ -43,6 +67,7 @@ void print(node *tail)
         printf("%d ", ptr -> data);
         ptr = ptr -> next;
     } while (ptr != tail -> next);
+    printf("\n");
 }
 
 
@@ -55,6 +80,10 @@ int main()
     tail = addAtEnd(tail, 6);
     tail = addAtEnd(tail, 56);
     
+    print(tail);
+
+    tail = addAtAnyPos(tail, 66, 4);
+
     print(tail);
     return 0;
 }
