1f085cbd9b8b48dfed988df03762fcf618530e20
ShivamJhaXXIII <shivam23040223@gmail.com>
2024-06-07 09:39:20 +0530

    idk

--- Assignment/AdditionaMultiplication.c
+++ Assignment/AdditionaMultiplication.c
@@ -2,8 +2,8 @@
 
 
 int main() {
-    int row,column,row1,column1;
-    printf("Enter the order of first matrix: ");
+    int row,column;
+    printf("Enter the order of the matrix: ");
     scanf("%d %d", &row, &column);
     int first[row][column];
     for(int i  = 0; i < row; i++) {
@@ -12,21 +12,52 @@ int main() {
             scanf("%d", &first[i][j]);
         }
     }
-
-    for(int i = 0; i < column; i++) {
-        for(int j = 0; j < row; j++) {
-            printf("%d ", first[i][j]);
-        }
-        printf("\n");
-    }
-
-    printf("Enter the order o")
-    int second[row1][column1];
-    for(int i  = 0; i < row1; i++) {
-        for(int j = 0; j < column1; j++) {
+    
+    // printf("First matrix:\n");
+    // for(int i = 0; i < column; i++) {
+    //     for(int j = 0; j < row; j++) {
+    //         printf("%d ", first[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+    
+    
+    printf("Enter the Element of second matrix\n");
+    int second[row][column];
+    for(int i  = 0; i < row; i++) {
+        for(int j = 0; j < column; j++) {
             printf("Enter the element: ");
             scanf("%d", &second[i][j]);
         }
     }
+    
+    // printf("Second matrix:\n");
+    // for(int i = 0; i < column; i++) {
+    //     for(int j = 0; j < row; j++) {
+    //         printf("%d ", first[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+    // int add[row][column];
+    // for(int i = 0; i < row; i++) {
+    //     for(int j = 0; j < column; j++) {
+    //         add[i][j] = first[i][j] + second[i][j];
+    //     }
+    // }
 
+    int mult[row][column];
+    for(int i = 0; i < row; i++) {
+        for(int j = 0; j < column; j++) {
+            mult[i][j] = first[i][j] * second[i][j];
+        }
+    }
+
+    printf("Multiplication of the two matrix is:\n");
+    for(int i = 0; i < column; i++) {
+        for(int j = 0; j < row; j++) {
+            printf("%d ", mult[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
 }
